77fc470b71a909588daec4e810f79969ee4c47cf
Thuy Nguyen <ngtan3@gmail.com>
2020-04-07 19:50:37 -0500

    intro to js

---
+++ age_calculator/calculator.js
@@ -0,0 +1,7 @@
+var age = prompt("How old are you?");
+var calculator = age * 365.25; //leap year include .25
+
+
+// alert("You have been alive for about " + calculator + " " + "days")
+
+alert(age + " years is roughly " + calculator + " days");
