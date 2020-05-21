33e3c3c6dba8f9dad5fb775cb55dcfa7f23faa6c
Thuy Nguyen <ngtan3@gmail.com>
2020-05-21 16:21:40 -0500

    score keeper part2

--- score_keeper/scores.js
+++ score_keeper/scores.js
@@ -1,5 +1,6 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
+var resetButton = document.querySelector("#reset"); 
 
 var p1Display = document.querySelector("#p1Display"); 
 var p2Dsiplay = document.querySelector("#p2Display");
@@ -14,6 +15,7 @@ p1Button.addEventListener("click", function(){
     if(!gameOver){
         p1Score++;
         if(p1Score === winningScore){
+            p1Display.classList.add("winner");
             gameOver = true;
         }
         p1Display.textContent = p1Score;
@@ -24,9 +26,19 @@ p2Button.addEventListener("click", function(){
     if(!gameOver){
         p2Score++;
         if(p2Score === winningScore){
+            p2Dsiplay.classList.add("winner");
             gameOver = true;
         }
         p2Dsiplay.textContent = p2Score;
     }
 });
 
+resetButton.addEventListener("click", function(){
+    p1Score = 0;
+    p2Score = 0; 
+    p1Display.textContent = 0;
+    p2Dsiplay.textContent = 0; 
+    p1Display.classList.remove("winner");
+    p2Dsiplay.classList.remove("winner");
+    gameOver = false;
+})
