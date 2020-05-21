ab7f6ea9fe05e55c21236c3084d6a1363559332a
Thuy Nguyen <ngtan3@gmail.com>
2020-05-21 16:53:47 -0500

    score keeper part 2 with numInput

--- score_keeper/scores.js
+++ score_keeper/scores.js
@@ -1,7 +1,9 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
 var resetButton = document.querySelector("#reset"); 
-
+var numInput = document.querySelector("input");
+var p = document.querySelector("p");
+var winningScoreDisplay = document.querySelector("p span");
 var p1Display = document.querySelector("#p1Display"); 
 var p2Dsiplay = document.querySelector("#p2Display");
 
@@ -34,6 +36,10 @@ p2Button.addEventListener("click", function(){
 });
 
 resetButton.addEventListener("click", function(){
+    reset();
+})
+
+function reset(){
     p1Score = 0;
     p2Score = 0; 
     p1Display.textContent = 0;
@@ -41,4 +47,10 @@ resetButton.addEventListener("click", function(){
     p1Display.classList.remove("winner");
     p2Dsiplay.classList.remove("winner");
     gameOver = false;
-})
+}
+
+numInput.addEventListener("change", function(){
+    winningScoreDisplay.textContent = numInput.value;
+    winningScore = Number(numInput.value);
+    reset();
+});
