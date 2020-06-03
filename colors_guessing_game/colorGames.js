09a69e05c576f4e34a38b9f2a49d2ff939076615
Thuy Nguyen <ngtan3@gmail.com>
2020-06-02 23:51:51 -0500

    color game part 5

--- colors_guessing_game/colorGames.js
+++ colors_guessing_game/colorGames.js
@@ -5,6 +5,16 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector('#message');
 var h1 = document.querySelector("h1");
 
+var resetButton = document.querySelector("#reset");resetButton.addEventListener("click", function(){
+	colors = generateRandomColors(6);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i=0; i < squares.length; i++){
+		squares[i].style.backgroundColor = colors[i]
+	}
+	h1.style.backgroundColor = "#232323";
+})
+
 colorDisplay.textContent = pickedColor; 
 
 for(var i=0; i<squares.length; i++){
@@ -19,6 +29,7 @@ for(var i=0; i<squares.length; i++){
 		//console.log(clickedColor, pickedColor)
         if(clickedColor === pickedColor){
 			messageDisplay.textContent = "Correct!";
+			resetButton.textContent = "Play Again?";
 			changeColors(clickedColor);
 			// when click on correct color - background of h1 change to match correct color 
 			h1.style.backgroundColor = clickedColor;
