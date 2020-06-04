c2f427b55bf8dfda23b7cb74805024e977c896d6
Thuy Nguyen <ngtan3@gmail.com>
2020-06-04 14:35:38 -0500

    alternating easy and hard button selection with color button

--- colors_guessing_game/colorGames.js
+++ colors_guessing_game/colorGames.js
@@ -4,8 +4,23 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector('#message');
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 
-var resetButton = document.querySelector("#reset");resetButton.addEventListener("click", function(){
+easyBtn.addEventListener("click", function(){
+	hardBtn.classList.remove("selected");
+	easyBtn.classList.add("selected");
+});
+
+hardBtn.addEventListener("click", function(){
+	hardBtn.classList.add("selected");
+	easyBtn.classList.remove("selected");
+});
+
+
+
+resetButton.addEventListener("click", function(){
 	colors = generateRandomColors(6);
 	pickedColor = pickColor();
 	colorDisplay.textContent = pickedColor;
